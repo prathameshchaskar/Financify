c4c5b358e5c52f76a0121f17dab258bc5438624e
Prathamesh Chaskar <prathameshchaskar1@gmail.com>
2024-08-01 00:16:06 +0530

    react firebase hooks added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
-import "./styles.css"
+import React, { useEffect } from "react";
+import "./styles.css";
+import { auth } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { signOut } from "firebase/auth";
 
 export const Header = () => {
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user, loading]);
 
   const logoutFun = () => {
-    alert("logout!");
-  }
-
+    try {
+      signOut(auth)
+        .then(() => {
+          // Sign-out successful.
+          toast.success("Logged out successfully");
+          navigate("/")
+        })
+        .catch((error) => {
+          // An error happened.
+          toast.error(error.message);
+        });
+    } catch (e) {
+      toast.error(e.message);
+    }
+  };
 
   return (
-    <div className='navbar'>
-      <p className='logo'>Financify</p>
-      <p className='logo link' onClick={logoutFun}>Logout</p>
+    <div className="navbar">
+      <p className="logo">Financify</p>
+      {user && (
+        <p className="logo link" onClick={logoutFun}>
+          Logout
+        </p>
+      )}
     </div>
-
-    
-  )
-}
+  );
+};
