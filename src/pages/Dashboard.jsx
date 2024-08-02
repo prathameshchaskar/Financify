3f4638f693e8e59b01ee4fbc1b62c5ba3c2c0e4d
Prathamesh Chaskar <prathameshchaskar1@gmail.com>
2024-08-02 14:26:43 +0530

    added transactions to firebase

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,8 +4,15 @@ import { Cards } from "../components/Cards";
 import { Modal } from "antd";
 import AddExpenseModal from "../components/Modals/addExpense";
 import AddIncomeModal from "../components/Modals/addIncome";
+import moment from "moment";
+import { toast } from "react-toastify";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 const Dashboard = () => {
+  const[user] = useAuthState(auth);
+
   const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
 
@@ -26,7 +33,29 @@ const Dashboard = () => {
   };
 
   const onFinish = (values, type) => {
-    console.log("on finish", values, type );
+    const newTransaction = {
+      type: type,
+      date: moment(values.data).format("YYYY-MM-DD"),
+      amount: parseFloat(values.amount),
+      tag: values.tag,
+      names: values.name,
+    };
+    addTransaction(newTransaction);
+  }
+
+  const addTransaction = async(transaction) => {
+    try {
+      const docRef = await addDoc(
+        collection(db,`users/${user.uid}/transation`),
+        transaction
+      );
+      console.log("Document written with ID: ",docRef.id);
+      toast.success("Transaction Added!");
+    }
+    catch (e) {
+      console.error("Error adding document: ", e);
+      toast.error("Couldn't add transaction");
+    }
   }
 
   return (
