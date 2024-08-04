2ab71304d756c4eff4df15e1542c0d4c4569b3b8
Prathamesh Chaskar <prathameshchaskar1@gmail.com>
2024-08-05 00:31:47 +0530

    sorting functionalities done and styling fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,6 +5,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { signOut } from "firebase/auth";
+import userSvg from "../../assets/user.svg"
 
 export const Header = () => {
   const [user, loading] = useAuthState(auth);
@@ -37,9 +38,18 @@ export const Header = () => {
     <div className="navbar">
       <p className="logo">Financify</p>
       {user && (
+        <div style={{display:"flex", alignItems:"center", gap:"1rem"}}>
+    
+            <img
+              src={user.photoURL ? user.photoURL : userSvg}
+              width={user.photoURL ? "30" : "24"}
+              style={{ borderRadius: "50%" }}
+            />
+         
         <p className="logo link" onClick={logoutFun}>
           Logout
         </p>
+        </div>
       )}
     </div>
   );
--- src/components/TransactionsTable/index.jsx
+++ src/components/TransactionsTable/index.jsx
@@ -1,9 +1,12 @@
-import { Select, Table } from "antd";
+import { Radio, Select, Table } from "antd";
 import React, { useState } from "react";
+import searchImg from "../../assets/search.svg";
+import "./styles.css";
 
 const TransactionsTable = ({ transactions }) => {
   const [search, setSearch] = useState("");
   const [typeFilter, setTypeFilter] = useState("");
+  const [sortKey, setSortKey] = useState("");
   const columns = [
     {
       title: "Name",
@@ -39,25 +42,103 @@ const TransactionsTable = ({ transactions }) => {
     );
   });
 
+  let sortedTransactions = filteredTransactions.sort((a, b) => {
+    if (sortKey === "date") {
+      return new Date(a.date) - new Date(b.date);
+    } else if (sortKey === "amount") {
+      return a.amount - b.amount;
+    } else {
+      return 0;
+    }
+  });
+
   return (
     <>
-      <input
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search by name"
-      />
-      <Select
-        className="select-input"
-        value={typeFilter}
-        onChange={(value) => setTypeFilter(value)}
-        placeholder="Select type"
-        allowClear
+      <div
+        style={{
+          padding: "0rem 2rem",
+        }}
       >
-        <Select.Option value="">All</Select.Option>
-        <Select.Option value="income">Income</Select.Option>
-        <Select.Option value="expense">Expense</Select.Option>
-      </Select>
-      <Table columns={columns} dataSource={filteredTransactions} />;
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            gap: "1rem",
+            alignItems: "center",
+            marginBottom: "1rem",
+          }}
+        >
+          <div className="input-flex">
+            <img src={searchImg} width="16" />
+            <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search by name"
+            />
+          </div>
+          <Select
+            className="select-input"
+            value={typeFilter}
+            onChange={(value) => setTypeFilter(value)}
+            placeholder="Select type"
+            allowClear
+          >
+            <Select.Option value="">All</Select.Option>
+            <Select.Option value="income">Income</Select.Option>
+            <Select.Option value="expense">Expense</Select.Option>
+          </Select>
+        </div>
+        <div className="my-table">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "100%",
+              marginBottom: "1rem",
+            }}
+          >
+            <h2 style={{fontWeight:"500"}}>My Transactions</h2>
+            <Radio.Group
+              className="input-radio"
+              onChange={(e) => setSortKey(e.target.value)}
+              value={sortKey}
+            >
+              <Radio.Button value="">No Sort</Radio.Button>
+              <Radio.Button value="date">Sort by Date</Radio.Button>
+              <Radio.Button value="amount">Sort by Amount</Radio.Button>
+            </Radio.Group>
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                gap: "1rem",
+                width: "400px",
+              }}
+            >
+              <button
+                className="btn"
+                // onClick={exportToCsv}
+              >
+                Export to CSV
+              </button>
+              <label for="file-csv" className="btn btn-blue">
+                Import from CSV
+              </label>
+              <input
+                // onChange={importFromCsv}
+                id="file-csv"
+                type="file"
+                accept=".csv"
+                required
+                style={{ display: "none" }}
+              />
+            </div>
+          </div>
+          <Table columns={columns} dataSource={filteredTransactions} />;
+        </div>
+      </div>
     </>
   );
 };
