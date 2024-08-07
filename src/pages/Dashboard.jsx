be4ac4332b0558198bb826eeb6b6d2f0d27ca05c
Prathamesh Chaskar <prathameshchaskar1@gmail.com>
2024-08-07 14:43:19 +0530

    charts added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,6 +8,10 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
 import { addDoc, collection, doc, getDocs, query } from "firebase/firestore";
 import TransactionsTable from "../components/TransactionsTable";
+import { Card, Row } from "antd";
+import NoTransactions from "../components/NoTransactions";
+import moment from "moment";
+import { Line, Pie } from "@ant-design/charts";
 
 const Dashboard = () => {
   const [user] = useAuthState(auth);
@@ -20,6 +24,48 @@ const Dashboard = () => {
   const [income, setIncome] = useState(0);
   const [expense, setExpense] = useState(0);
   const [totalBalance, setTotalBalance] = useState(0);
+
+
+  const processChartData = () => {
+    const balanceData = [];
+    const spendingData = {};
+
+    transactions.forEach((transaction) => {
+      const monthYear = moment(transaction.date).format("MMM YYYY");
+      const tag = transaction.tag;
+
+      if (transaction.type === "income") {
+        if (balanceData.some((data) => data.month === monthYear)) {
+          balanceData.find((data) => data.month === monthYear).balance +=
+            transaction.amount;
+        } else {
+          balanceData.push({ month: monthYear, balance: transaction.amount });
+        }
+      } else {
+        if (balanceData.some((data) => data.month === monthYear)) {
+          balanceData.find((data) => data.month === monthYear).balance -=
+            transaction.amount;
+        } else {
+          balanceData.push({ month: monthYear, balance: -transaction.amount });
+        }
+
+        if (spendingData[tag]) {
+          spendingData[tag] += transaction.amount;
+        } else {
+          spendingData[tag] = transaction.amount;
+        }
+      }
+    });
+
+    const spendingDataArray = Object.keys(spendingData).map((key) => ({
+      category: key,
+      value: spendingData[key],
+    }));
+
+    return { balanceData, spendingDataArray };
+  };
+
+  const { balanceData, spendingDataArray } = processChartData();
 
   const showIncomeModal = () => {
     setIsIncomeModalVisible(true);
@@ -107,6 +153,25 @@ const Dashboard = () => {
     setLoading(false);
   };
 
+  const balanceConfig = {
+    data: balanceData,
+    xField: "month",
+    yField: "balance",
+  };
+
+  const spendingConfig = {
+    data: spendingDataArray,
+    angleField: "value",
+    colorField: "category",
+  };
+  const cardStyle = {
+    boxShadow: "0px 0px 30px 8px rgba(227, 227, 227, 0.75)",
+    margin: "2rem",
+    borderRadius: "0.5rem",
+    minWidth: "400px",
+    flex: 1,
+  };
+
   return (
     <div>
       <Header />
@@ -131,6 +196,27 @@ const Dashboard = () => {
             handleIncomeCancel={handleIncomeCancel}
             onFinish={onFinish}
           />
+          {transactions.length === 0 ? (
+            <NoTransactions />
+          ) : (
+            <>
+              <Row gutter={16}>
+                <Card bordered={true} style={cardStyle}>
+                  <h2>Financial Statistics</h2>
+                  <Line {...{ ...balanceConfig, data: balanceData }} />
+                </Card>
+
+                <Card bordered={true} style={{ ...cardStyle, flex: 0.45 }}>
+                  <h2>Total Spending</h2>
+                  {spendingDataArray.length == 0 ? (
+                    <p>Seems like you haven't spent anything till now...</p>
+                  ) : (
+                    <Pie {...{ ...spendingConfig, data: spendingDataArray }} />
+                  )}
+                </Card>
+              </Row>
+            </>
+          )}
           <TransactionsTable
             transactions={transactions}
             addTransaction={addTransaction}
